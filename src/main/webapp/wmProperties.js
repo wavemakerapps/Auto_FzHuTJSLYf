9b113065ca9a1a45ab22361f807caf23bdfae978
root <root@61949b3d2f95>
2017-07-04 17:59:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_FzHuTJSLYf",
+  "homePage" : "Main",
+  "name" : "Auto_FzHuTJSLYf",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
